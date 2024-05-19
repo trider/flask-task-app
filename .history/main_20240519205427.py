0f82c7fc0c0eacf8774513fd713356508d8ef71b
Jonathan Gold <jgold@instate.io>
2024-05-19 20:56:29 +0300

    update

--- .history/main_20240519205427.py
+++ .history/main_20240519205427.py
@@ -17,7 +17,7 @@ if __name__ == "__main__":
   print('****************************************')
   print('flask-task-app: Ver', '0.1.1')
   print('****************************************\n')
-  app.run(host='0.0.0.0', port=5001, debug=True)
+  app.run(host='0.0.0.0', port=5000, debug=True)
 
 
 
